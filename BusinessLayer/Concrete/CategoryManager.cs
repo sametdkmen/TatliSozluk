83152a7af58876e8f297a4841e617acc01e35716
Samet Dikmen <sametdikmen1905@gmai.com>
2022-11-29 12:55:00 +0300

    değişikler

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,25 +10,47 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDal _categoryDal;
 
-        public List<Category> GetAllBL() // Listeleme için Category tablosundakileri getirmek için metot oluşturdum.
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            return repo.List();
+            _categoryDal = categoryDal;
         }
 
-        public void CategoryAddBL(Category p) // Category sınıfından p parametresi gönderilecek.
+        public void CategoryAdd(Category category)
         {
-            if (p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)  // Şartlarımı, kısıtlamalarımı yazdım. 
-            {
-                //hata mesajı
-            }
-            else
-            {
-                repo.Insert(p);  // Yukarıda kısıtlamalara takılmayan girişler buradan eklenecek.
-            }
+            _categoryDal.Insert(category);
         }
+
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+
+
+
     }
+    
+    // ESKİ KODLAR 
+        
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        //public List<Category> GetAllBL() // Listeleme için Category tablosundakileri getirmek için metot oluşturdum.
+        //{
+        //    return repo.List();
+        //}
+
+        //public void CategoryAddBL(Category p) // Category sınıfından p parametresi gönderilecek.
+        //{
+        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)  // Şartlarımı, kısıtlamalarımı yazdım. 
+        //    {
+        //        //hata mesajı
+        //    }
+        //    else
+        //    {
+        //        repo.Insert(p);  // Yukarıda kısıtlamalara takılmayan girişler buradan eklenecek.
+        //    }
+        //}
 }
--- TatliSozluk.UI.WEB/Controllers/CategoryController.cs
+++ TatliSozluk.UI.WEB/Controllers/CategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +14,43 @@ namespace TatliSozluk.UI.WEB.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+
+        [HttpGet] // Sayfa yüklendiği zaman sadece sayfayi geri döndürecek, aşağıda butona tıklandıktan sonra post işlemine gidecek.
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost] // Ben sayfada bir butona tıkladığımda post edildiğinde bu çalışacak htppost attributeü sayesinde.
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();  //Ekleme işlemi sonrası beni tekrar listeleme işleminin olduğu metoda yönlendir dedim.
         }
     }
 }
