d7bd16479bd03fec4453ba829a3d04cce9777361
Samet Dikmen <sametdikmen1905@gmai.com>
2022-11-29 13:17:49 +0300

    up+

--- BusinessLayer/ValidationRules/CategoryValidator.cs
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -12,10 +12,10 @@ namespace BusinessLayer.ValidationRules
     {
         public CategoryValidator()        // Veri girişi için kuralları buraya yazıyoruz.
         {
-            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz");
-            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı Boş Geçemezsiniz.");
-            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz.");
-            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın.");
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz"); /*Boş geçememeleri için NotEmpty() kullanılıyor.*/
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı Boş Geçemezsiniz."); /*Boş geçememeleri için NotEmpty() kullanılıyor.*/
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz."); /* Girilebilecek en az karakteri belirlemek için MinimumLength(n) kullanılıyor.*/
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın."); /* Girilebilecek en fazla karakteri belirlemek için MaximumLength(n) kullanılıyor.*/
         }
     }
 }
